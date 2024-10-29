87510b0775893effa9f29ccd36c1656bd5ef262e
Tran Le Phuong Tran <trantlp0911@gmail.com>
2024-10-29 14:44:19 +0700

    done header footer final final

--- furni-1.0.0/footer.php
+++ furni-1.0.0/footer.php
@@ -3,20 +3,18 @@
     <div class="container relative">
 
         <div class="sofa-img">
-<<<<<<< HEAD
-            <img src="images/sach.png" alt="Image" class="img-fluid">
-=======
-            <img src="images/sofa.png" alt="Image" class="img-fluid">
->>>>>>> bb4b4cc (done header footer)
+            <<<<<<< HEAD <img src="images/sach.png" alt="Image" class="img-fluid">
+                =======
+                <img src="images/sofa.png" alt="Image" class="img-fluid">
+                >>>>>>> bb4b4cc (done header footer)
         </div>
 
         <div class="row">
             <div class="col-lg-8">
                 <div class="subscription-form">
                     <h3 class="d-flex align-items-center"><span class="me-1"><img src="images/envelope-outline.svg"
-<<<<<<< HEAD
-<<<<<<< HEAD
-                                alt="Image" class="img-fluid"></span><span>Liên hệ với chúng tôi</span></h3>
+                                <<<<<<< HEAD <<<<<<< HEAD alt="Image" class="img-fluid"></span><span>Liên hệ với
+                            chúng tôi</span></h3>
 
                     <form action="#" class="row g-3">
                         <div class="col-auto">
@@ -24,37 +22,35 @@
                         </div>
                         <div class="col-auto">
                             <input type="email" class="form-control" placeholder="Email của bạn">
-=======
-                                alt="Image" class="img-fluid"></span><span>Subscribe to Newsletter</span></h3>
-=======
-                                alt="Image" class="img-fluid"></span><span>Liên hệ với chúng tôi</span></h3>
->>>>>>> 6e38dd3 (done header footer final)
+                            =======
+                            alt="Image" class="img-fluid"></span><span>Subscribe to Newsletter</span></h3>
+                            =======
+                            alt="Image" class="img-fluid"></span><span>Liên hệ với chúng tôi</span></h3>
+                            >>>>>>> 6e38dd3 (done header footer final)
 
-                    <form action="#" class="row g-3">
-                        <div class="col-auto">
-                            <input type="text" class="form-control" placeholder="Tên của bạn">
-                        </div>
-                        <div class="col-auto">
-<<<<<<< HEAD
-                            <input type="email" class="form-control" placeholder="Enter your email">
->>>>>>> bb4b4cc (done header footer)
-=======
-                            <input type="email" class="form-control" placeholder="Email của bạn">
->>>>>>> 6e38dd3 (done header footer final)
-                        </div>
-                        <div class="col-auto">
-                            <button class="btn btn-primary">
-                                <span class="fa fa-paper-plane"></span>
-                            </button>
-                        </div>
-                    </form>
+                            <form action="#" class="row g-3">
+                                <div class="col-auto">
+                                    <input type="text" class="form-control" placeholder="Tên của bạn">
+                                </div>
+                                <div class="col-auto">
+                                    <<<<<<< HEAD <input type="email" class="form-control"
+                                        placeholder="Enter your email">
+                                        >>>>>>> bb4b4cc (done header footer)
+                                        =======
+                                        <input type="email" class="form-control" placeholder="Email của bạn">
+                                        >>>>>>> 6e38dd3 (done header footer final)
+                                </div>
+                                <div class="col-auto">
+                                    <button class="btn btn-primary">
+                                        <span class="fa fa-paper-plane"></span>
+                                    </button>
+                                </div>
+                            </form>
 
+                        </div>
                 </div>
             </div>
-        </div>
 
-<<<<<<< HEAD
-        <div style="font-size: larger">
             <div class="row g-7 mb-7">
                 <div class="col-lg-4">
                     <div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">Nhà sách A Plus</a>
@@ -73,26 +69,26 @@
 
                 <div class="col-lg-4">
                     <div class="row links-wrap">
-                        <div class="col-12 col-sm-6 col-md-6">
+                        <div class="col-12 col-sm-6 col-md-4">
                             <ul class="list-unstyled">
                                 <li>
-                                    <div style='position: relative; left: 30px;'><a href="#">Giới thiệu</a></div>
+                                    <div style='position: relative; left: 50px;'><a href="#">Giới thiệu</a></div>
                                 </li>
                                 <li>
-                                    <div style='position: relative; left: 30px;'>
+                                    <div style='position: relative; left: 50px;'>
                                         <a href="#">Chính sách</a>
                                     </div>
                                 </li>
                             </ul>
                         </div>
 
-                        <div class="col-6 col-sm-6 col-md-6">
+                        <div class="col-6 col-sm-6 col-md-3">
                             <ul class="list-unstyled">
                                 <li>
-                                    <div style='position: relative; left: 10px;'><a href="#">Sản phẩm</a></div>
+                                    <div style='position: relative; left: 100px;'><a href="#">Sản phẩm</a></div>
                                 </li>
                                 <li>
-                                    <div style='position: relative; left: 10px;'><a href="#">Giỏ hàng</a></div>
+                                    <div style='position: relative; left: 100px;'><a href="#">Giỏ hàng</a></div>
                                 </li>
                             </ul>
 
@@ -106,91 +102,36 @@
                     giúp tiết kiệm chi phí và bảo vệ môi trường. Đến ngay A Plus để thỏa mãn đam mê đọc sách và khám
                     phá!
                 </div>
-=======
-        <div class="row g-7 mb-7">
-            <div class="col-lg-4">
-                <div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">Nhà sách A Plus</a>
-                </div>
-                <p class="mb-4">Chúng tôi tự hào là nơi cung cấp dịch vụ cho thuê sách đa dạng, từ truyện tranh đến các
-                    đầu sách chuyên ngành và tạp chí, phù hợp với mọi lứa tuổi và sở thích.</p>
-
-                <ul class="list-unstyled custom-social">
-                    <li><a href="#"><span class="fa fa-brands fa-facebook-f"></span></a></li>
-                    <li><a href="#"><span class="fa fa-brands fa-twitter"></span></a></li>
-                    <li><a href="#"><span class="fa fa-brands fa-instagram"></span></a></li>
-                    <li><a href="#"><span class="fa fa-brands fa-linkedin"></span></a></li>
-                </ul>
             </div>
 
-            <div class="col-lg-4">
-                <div class="row links-wrap">
-                    <div class="col-12 col-sm-6 col-md-3">
-                        <ul class="list-unstyled">
-                            <li>
-                                <div style='position: relative; left: 80px;'><a href="#">Giới thiệu</a></div>
-                            </li>
-                            <li>
-                                <div style='position: relative; left: 80px;'>
-                                    <a href="#">Chính sách</a>
-                                </div>
-                            </li>
+            <div class="border-top copyright">
+                <div class="row pt-4">
+                    <div class="col-lg-6">
+                        <p class="mb-2 text-center text-lg-start">Copyright &copy;
+                            <script>document.write(new Date().getFullYear());</script>. All Rights Reserved. &mdash;
+                            <<<<<<< HEAD <<<<<<< HEAD Designed with love by A Plus. Distributed By A Plus=======Designed
+                                with love by <a href="https://untree.co">Untree.co</a> Distributed By <a
+                                    hreff="https://themewagon.com">ThemeWagon</a>
+                                <!-- License information: https://untree.co/license/ -->
+                                >>>>>>> bb4b4cc (done header footer)
+                                =======
+                                Designed with love by A Plus. Distributed By A Plus
+
+                                >>>>>>> 6e38dd3 (done header footer final)
+                        </p>
+                    </div>
+
+                    <div class="col-lg-6 text-center text-lg-end">
+                        <ul class="list-unstyled d-inline-flex ms-auto">
+                            <li class="me-4"><a href="#">Terms &amp; Conditions</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
                         </ul>
                     </div>
 
-                    <div class="col-6 col-sm-6 col-md-3">
-                        <ul class="list-unstyled">
-                            <li>
-                                <div style='position: relative; left: 120px;'><a href="#">Sản phẩm</a></div>
-                            </li>
-                            <li>
-                                <div style='position: relative; left: 120px;'><a href="#">Giỏ hàng</a></div>
-                            </li>
-                        </ul>
-
-                    </div>
                 </div>
             </div>
 
-            <div class="col-lg-4">
-                <div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">Tại A Plus</a>
-                </div>Bạn sẽ dễ dàng tìm thấy những tác phẩm yêu thích mà không cần phải mua,
-                giúp tiết kiệm chi phí và bảo vệ môi trường. Đến ngay A Plus để thỏa mãn đam mê đọc sách và khám phá!
->>>>>>> bb4b4cc (done header footer)
-            </div>
         </div>
-
-        <div class="border-top copyright">
-            <div class="row pt-4">
-                <div class="col-lg-6">
-                    <p class="mb-2 text-center text-lg-start">Copyright &copy;
-                        <script>document.write(new Date().getFullYear());</script>. All Rights Reserved. &mdash;
-<<<<<<< HEAD
-<<<<<<< HEAD
-                        Designed with love by A Plus. Distributed By A Plus
-
-=======
-                        Designed with love by <a href="https://untree.co">Untree.co</a> Distributed By <a
-                            hreff="https://themewagon.com">ThemeWagon</a>
-                        <!-- License information: https://untree.co/license/ -->
->>>>>>> bb4b4cc (done header footer)
-=======
-                        Designed with love by A Plus. Distributed By A Plus
-
->>>>>>> 6e38dd3 (done header footer final)
-                    </p>
-                </div>
-
-                <div class="col-lg-6 text-center text-lg-end">
-                    <ul class="list-unstyled d-inline-flex ms-auto">
-                        <li class="me-4"><a href="#">Terms &amp; Conditions</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
-                    </ul>
-                </div>
-
-            </div>
-        </div>
-
-    </div>
 </footer>
 <!-- End Footer Section -->
 
