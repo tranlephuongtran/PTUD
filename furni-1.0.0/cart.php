8af435370e6e09c1f93e063860d21adcab4d0723
Tran Le Phuong Tran <trantlp0911@gmail.com>
2024-10-29 13:57:20 +0700

    change to php file

--- furni-1.0.0/cart.php
+++ furni-1.0.0/cart.php
@@ -38,24 +38,23 @@ include('header.php')
 					<table class="table">
 						<thead>
 							<tr>
-								<th class="product-thumbnail">Hình Ảnh</th>
-								<th class="product-name">Tên Sách</th>
-								<th class="product-price">Giá Thuê</th>
-								<th class="product-quantity">Số lượng</th>
-								<th class="product-total">Tiền cọc</th>
-								<th class="product-remove">Thao tác</th>
+								<th class="product-thumbnail">Image</th>
+								<th class="product-name">Product</th>
+								<th class="product-price">Price</th>
+								<th class="product-quantity">Quantity</th>
+								<th class="product-total">Total</th>
+								<th class="product-remove">Remove</th>
 							</tr>
 						</thead>
-						<tbody style="font-size: larger">
+						<tbody>
 							<tr>
 								<td class="product-thumbnail">
-									<img src="images/CayCamNgotCuaToi.png" style="width: 120px;height: 160px;"
-										alt="Image" class="img-fluid">
+									<img src="images/product-1.png" alt="Image" class="img-fluid">
 								</td>
 								<td class="product-name">
-									<h2 class="h5 text-black">Cây Cam Ngọt Của Tôi</h2>
+									<h2 class="h5 text-black">Product 1</h2>
 								</td>
-								<td>17.000 VND</td>
+								<td>$49.00</td>
 								<td>
 									<div class="input-group mb-3 d-flex align-items-center quantity-container"
 										style="max-width: 120px;">
@@ -72,19 +71,18 @@ include('header.php')
 									</div>
 
 								</td>
-								<td>102.000 VND</td>
-								<td><a href="#" class="btn btn-black btn-sm"><button>Xóa</button></a></td>
+								<td>$49.00</td>
+								<td><a href="#" class="btn btn-black btn-sm">X</a></td>
 							</tr>
 
 							<tr>
 								<td class="product-thumbnail">
-									<img src="images/KhongGiaDinh.png" style="width: 120px;height: 160px;" alt=" Image"
-										class="img-fluid">
+									<img src="images/product-2.png" alt="Image" class="img-fluid">
 								</td>
 								<td class="product-name">
-									<h2 class="h5 text-black">Không Gia Đình</h2>
+									<h2 class="h5 text-black">Product 2</h2>
 								</td>
-								<td>35.000 VND</td>
+								<td>$49.00</td>
 								<td>
 									<div class="input-group mb-3 d-flex align-items-center quantity-container"
 										style="max-width: 120px;">
@@ -101,8 +99,8 @@ include('header.php')
 									</div>
 
 								</td>
-								<td>210.000 VND</td>
-								<td><a href="#" class="btn btn-black btn-sm"><button>Xóa</button></a></td>
+								<td>$49.00</td>
+								<td><a href="#" class="btn btn-black btn-sm">X</a></td>
 							</tr>
 						</tbody>
 					</table>
@@ -112,45 +110,56 @@ include('header.php')
 
 		<div class="row">
 			<div class="col-md-6">
-				<!-- TRỐNG ND-->
+				<div class="row mb-5">
+					<div class="col-md-6 mb-3 mb-md-0">
+						<button class="btn btn-black btn-sm btn-block">Update Cart</button>
+					</div>
+					<div class="col-md-6">
+						<button class="btn btn-outline-black btn-sm btn-block">Continue Shopping</button>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<label class="text-black h4" for="coupon">Coupon</label>
+						<p>Enter your coupon code if you have one.</p>
+					</div>
+					<div class="col-md-8 mb-3 mb-md-0">
+						<input type="text" class="form-control py-3" id="coupon" placeholder="Coupon Code">
+					</div>
+					<div class="col-md-4">
+						<button class="btn btn-black">Apply Coupon</button>
+					</div>
+				</div>
 			</div>
 			<div class="col-md-6 pl-5">
 				<div class="row justify-content-end">
 					<div class="col-md-7">
 						<div class="row">
 							<div class="col-md-12 text-right border-bottom mb-5">
-								<h3 class="text-black h4 text-uppercase">Tổng</h3>
+								<h3 class="text-black h4 text-uppercase">Cart Totals</h3>
 							</div>
 						</div>
 						<div class="row mb-3">
 							<div class="col-md-6">
-								<span class="text-black">Tổng tiền cọc</span>
+								<span class="text-black">Subtotal</span>
 							</div>
 							<div class="col-md-6 text-right">
-								<strong class="text-black">312.000 VND</strong>
+								<strong class="text-black">$230.00</strong>
 							</div>
 						</div>
 						<div class="row mb-5">
 							<div class="col-md-6">
-								<span class="text-black">Tổng tiền thuê</span>
+								<span class="text-black">Total</span>
 							</div>
 							<div class="col-md-6 text-right">
-								<strong class="text-black">52.000 VND</strong>
-							</div>
-						</div>
-						<div class="row mb-5">
-							<div class="col-md-6">
-								<span class="text-black"><b>TỔNG TIỀN PHẢI TRẢ</b></span>
-							</div>
-							<div class="col-md-6 text-right">
-								<strong class="text-black">364.000 VND</strong>
+								<strong class="text-black">$230.00</strong>
 							</div>
 						</div>
 
 						<div class="row">
 							<div class="col-md-12">
 								<button class="btn btn-black btn-lg py-3 btn-block"
-									onclick="window.location='checkout.php'">THUÊ SÁCH</button>
+									onclick="window.location='checkout.html'">Proceed To Checkout</button>
 							</div>
 						</div>
 					</div>
@@ -160,11 +169,123 @@ include('header.php')
 	</div>
 </div>
 
-<?php
-include('footer.php')
-	?>
+
+<!-- Start Footer Section -->
+<footer class="footer-section">
+	<div class="container relative">
+
+		<div class="sofa-img">
+			<img src="images/sofa.png" alt="Image" class="img-fluid">
+		</div>
+
+		<div class="row">
+			<div class="col-lg-8">
+				<div class="subscription-form">
+					<h3 class="d-flex align-items-center"><span class="me-1"><img src="images/envelope-outline.svg"
+								alt="Image" class="img-fluid"></span><span>Subscribe to Newsletter</span></h3>
+
+					<form action="#" class="row g-3">
+						<div class="col-auto">
+							<input type="text" class="form-control" placeholder="Enter your name">
+						</div>
+						<div class="col-auto">
+							<input type="email" class="form-control" placeholder="Enter your email">
+						</div>
+						<div class="col-auto">
+							<button class="btn btn-primary">
+								<span class="fa fa-paper-plane"></span>
+							</button>
+						</div>
+					</form>
+
+				</div>
+			</div>
+		</div>
+
+		<div class="row g-5 mb-5">
+			<div class="col-lg-4">
+				<div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">Furni<span>.</span></a></div>
+				<p class="mb-4">Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus
+					malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique.
+					Pellentesque habitant</p>
+
+				<ul class="list-unstyled custom-social">
+					<li><a href="#"><span class="fa fa-brands fa-facebook-f"></span></a></li>
+					<li><a href="#"><span class="fa fa-brands fa-twitter"></span></a></li>
+					<li><a href="#"><span class="fa fa-brands fa-instagram"></span></a></li>
+					<li><a href="#"><span class="fa fa-brands fa-linkedin"></span></a></li>
+				</ul>
+			</div>
+
+			<div class="col-lg-8">
+				<div class="row links-wrap">
+					<div class="col-6 col-sm-6 col-md-3">
+						<ul class="list-unstyled">
+							<li><a href="#">About us</a></li>
+							<li><a href="#">Services</a></li>
+							<li><a href="#">Blog</a></li>
+							<li><a href="#">Contact us</a></li>
+						</ul>
+					</div>
+
+					<div class="col-6 col-sm-6 col-md-3">
+						<ul class="list-unstyled">
+							<li><a href="#">Support</a></li>
+							<li><a href="#">Knowledge base</a></li>
+							<li><a href="#">Live chat</a></li>
+						</ul>
+					</div>
+
+					<div class="col-6 col-sm-6 col-md-3">
+						<ul class="list-unstyled">
+							<li><a href="#">Jobs</a></li>
+							<li><a href="#">Our team</a></li>
+							<li><a href="#">Leadership</a></li>
+							<li><a href="#">Privacy Policy</a></li>
+						</ul>
+					</div>
+
+					<div class="col-6 col-sm-6 col-md-3">
+						<ul class="list-unstyled">
+							<li><a href="#">Nordic Chair</a></li>
+							<li><a href="#">Kruzo Aero</a></li>
+							<li><a href="#">Ergonomic Chair</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+		<div class="border-top copyright">
+			<div class="row pt-4">
+				<div class="col-lg-6">
+					<p class="mb-2 text-center text-lg-start">Copyright &copy;
+						<script>document.write(new Date().getFullYear());</script>. All Rights Reserved. &mdash;
+						Designed with love by <a href="https://untree.co">Untree.co</a> Distributed By <a
+							hreff="https://themewagon.com">ThemeWagon</a>
+						<!-- License information: https://untree.co/license/ -->
+					</p>
+				</div>
+
+				<div class="col-lg-6 text-center text-lg-end">
+					<ul class="list-unstyled d-inline-flex ms-auto">
+						<li class="me-4"><a href="#">Terms &amp; Conditions</a></li>
+						<li><a href="#">Privacy Policy</a></li>
+					</ul>
+				</div>
+
+			</div>
+		</div>
+
+	</div>
+</footer>
+<!-- End Footer Section -->
 
 
 <script src="js/bootstrap.bundle.min.js"></script>
 <script src="js/tiny-slider.js"></script>
 <script src="js/custom.js"></script>
+</body>
+
+</html>
