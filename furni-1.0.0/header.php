1e8006cb23115f138caf1a1b3c04c3e544758cab
KingHuy <huydaden41@gmail.com>
2024-11-02 22:58:03 +0700

    Update Title

---
+++ furni-1.0.0/header.php
@@ -0,0 +1,61 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="Untree.co">
+    <link rel="shortcut icon" href="favicon.png">
+
+    <meta name="description" content="" />
+    <meta name="keywords" content="bootstrap, bootstrap4" />
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link href="css/tiny-slider.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <title>A Plus BookStore</title>
+    <!-- Furni Free Bootstrap 5 Template for Furniture and Interior Design Websites by Untree.co -->
+</head>
+
+<body>
+
+    <!-- Start Header/Navigation -->
+
+    <nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
+
+        <div class="container">
+            <a class="navbar-brand" href="index.php">A Plus<span>.</span></a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni"
+                aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarsFurni">
+                <ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Trang chủ</a>
+                    </li>
+                    <li><a class="nav-link" href="shop.php">Sản phẩm</a></li>
+                    <!-- <li class="active"></li> -->
+                    <li><a class="nav-link" href="about.php">Giới thiệu</a></li>
+                    <li><a class="nav-link" href="services.php">Chính sách</a></li>
+                </ul>
+
+                <ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
+                    <li><a class="nav-link" href="#"><img src="images/user.svg"></a></li>
+                    <li><a class="nav-link" href="cart.php"><img src="images/cart.svg"></a></li>
+                </ul>
+            </div>
+        </div>
+
+    </nav>
+    <!-- End Header/Navigation -->
+    <script>
+        $(document).ready(function () {
+            var selector = '.custom-navbar-nav li';
+            $(selector).on('click', function () {
+                $(selector).addClass('active');
+            });
+        });
+    </script>
