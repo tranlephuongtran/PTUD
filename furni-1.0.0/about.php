d9361c01bd55eac6e74d7eac7ebe470072308c76
Tran Le Phuong Tran <trantlp0911@gmail.com>
2024-11-03 20:24:24 +0700

    done Tran

--- furni-1.0.0/about.php
+++ furni-1.0.0/about.php
@@ -244,6 +244,7 @@
 
 			<!-- Start Team Section -->
 			<!-- <div class="untree_co-section" style="position: relative; bottom: 120px">
+<<<<<<< HEAD
 				<div class="container">
 					<div class="row mb-5">
 						<div class="col-lg-5 mx-auto text-center">
@@ -560,16 +561,18 @@
 
 			<!-- Start Team Section -->
 			<div class="untree_co-section" style="position: relative; bottom: 120px">
+=======
+>>>>>>> 3149c0f (done Tran)
 				<div class="container">
 					<div class="row mb-5">
 						<div class="col-lg-5 mx-auto text-center">
 							<h2 class="section-title">Our Team</h2>
 						</div>
 					</div>
-					<div class="row">
+					<div class="row"> -->
 
-						<!-- Start Column 1 -->
-						<div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
+			<!-- Start Column 1 -->
+			<!-- <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
 							<img src="images/person_1.jpg" class="img-fluid mb-5">
 							<h3 clas><a href="#"><span class="">Lawson</span> Arnold</a></h3>
 							<span class="d-block position mb-4">CEO, Founder, Atty.</span>
@@ -580,11 +583,11 @@
 							<p class="mb-0"><a href="#" class="more dark">Learn More <span
 										class="icon-arrow_forward"></span></a>
 							</p>
-						</div>
-						<!-- End Column 1 -->
+						</div> -->
+			<!-- End Column 1 -->
 
-						<!-- Start Column 2 -->
-						<div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
+			<!-- Start Column 2 -->
+			<!-- <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
 							<img src="images/person_2.jpg" class="img-fluid mb-5">
 
 							<h3 clas><a href="#"><span class="">Jeremy</span> Walker</a></h3>
@@ -597,11 +600,11 @@
 										class="icon-arrow_forward"></span></a>
 							</p>
 
-						</div>
-						<!-- End Column 2 -->
+						</div> -->
+			<!-- End Column 2 -->
 
-						<!-- Start Column 3 -->
-						<div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
+			<!-- Start Column 3 -->
+			<!-- <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
 							<img src="images/person_3.jpg" class="img-fluid mb-5">
 							<h3 clas><a href="#"><span class="">Patrik</span> White</a></h3>
 							<span class="d-block position mb-4">CEO, Founder, Atty.</span>
@@ -612,11 +615,11 @@
 							<p class="mb-0"><a href="#" class="more dark">Learn More <span
 										class="icon-arrow_forward"></span></a>
 							</p>
-						</div>
-						<!-- End Column 3 -->
+						</div> -->
+			<!-- End Column 3 -->
 
-						<!-- Start Column 4 -->
-						<div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
+			<!-- Start Column 4 -->
+			<!-- <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
 							<img src="images/person_4.jpg" class="img-fluid mb-5">
 
 							<h3 clas><a href="#"><span class="">Kathryn</span> Ryan</a></h3>
@@ -630,8 +633,8 @@
 							</p>
 
 
-						</div>
-						<!-- End Column 4 -->
+						</div> -->
+			<!-- End Column 4
 
 
 
@@ -642,6 +645,7 @@
 	</div>
 </div>
 <!-- End Team Section -->
+<<<<<<< HEAD
 <<<<<<< HEAD
 
 
@@ -896,3 +900,8 @@
 include('footer.php')
 	?>
 >>>>>>> bb4b4cc (done header footer)
+=======
+			<?php
+			include('footer.php')
+				?>
+>>>>>>> 3149c0f (done Tran)
