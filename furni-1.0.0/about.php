3149c0fbe3a9ef8f29efaa9e8007d8ea35df1024
Tran Le Phuong Tran <trantlp0911@gmail.com>
2024-11-03 20:24:24 +0700

    done Tran

--- furni-1.0.0/about.php
+++ furni-1.0.0/about.php
@@ -241,17 +241,17 @@
 			<!-- End Testimonial Slider -->
 
 			<!-- Start Team Section -->
-			<div class="untree_co-section" style="position: relative; bottom: 120px">
+			<!-- <div class="untree_co-section" style="position: relative; bottom: 120px">
 				<div class="container">
 					<div class="row mb-5">
 						<div class="col-lg-5 mx-auto text-center">
 							<h2 class="section-title">Our Team</h2>
 						</div>
 					</div>
-					<div class="row">
+					<div class="row"> -->
 
-						<!-- Start Column 1 -->
-						<div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
+			<!-- Start Column 1 -->
+			<!-- <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
 							<img src="images/person_1.jpg" class="img-fluid mb-5">
 							<h3 clas><a href="#"><span class="">Lawson</span> Arnold</a></h3>
 							<span class="d-block position mb-4">CEO, Founder, Atty.</span>
@@ -262,11 +262,11 @@
 							<p class="mb-0"><a href="#" class="more dark">Learn More <span
 										class="icon-arrow_forward"></span></a>
 							</p>
-						</div>
-						<!-- End Column 1 -->
+						</div> -->
+			<!-- End Column 1 -->
 
-						<!-- Start Column 2 -->
-						<div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
+			<!-- Start Column 2 -->
+			<!-- <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
 							<img src="images/person_2.jpg" class="img-fluid mb-5">
 
 							<h3 clas><a href="#"><span class="">Jeremy</span> Walker</a></h3>
@@ -279,11 +279,11 @@
 										class="icon-arrow_forward"></span></a>
 							</p>
 
-						</div>
-						<!-- End Column 2 -->
+						</div> -->
+			<!-- End Column 2 -->
 
-						<!-- Start Column 3 -->
-						<div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
+			<!-- Start Column 3 -->
+			<!-- <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
 							<img src="images/person_3.jpg" class="img-fluid mb-5">
 							<h3 clas><a href="#"><span class="">Patrik</span> White</a></h3>
 							<span class="d-block position mb-4">CEO, Founder, Atty.</span>
@@ -294,11 +294,11 @@
 							<p class="mb-0"><a href="#" class="more dark">Learn More <span
 										class="icon-arrow_forward"></span></a>
 							</p>
-						</div>
-						<!-- End Column 3 -->
+						</div> -->
+			<!-- End Column 3 -->
 
-						<!-- Start Column 4 -->
-						<div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
+			<!-- Start Column 4 -->
+			<!-- <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
 							<img src="images/person_4.jpg" class="img-fluid mb-5">
 
 							<h3 clas><a href="#"><span class="">Kathryn</span> Ryan</a></h3>
@@ -312,8 +312,8 @@
 							</p>
 
 
-						</div>
-						<!-- End Column 4 -->
+						</div> -->
+			<!-- End Column 4
 
 
 
@@ -324,6 +324,6 @@
 	</div>
 </div>
 <!-- End Team Section -->
-<?php
-include('footer.php')
-	?>
+			<?php
+			include('footer.php')
+				?>
