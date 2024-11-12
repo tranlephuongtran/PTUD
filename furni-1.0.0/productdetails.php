7b0dcca30a5b6efd380f66f8ac7a4af6ca9c0b1c
your name <phamquan100503@gmail.com>
2024-11-12 13:05:47 +0700

    button chitietsanpham

--- furni-1.0.0/productdetails.php
+++ furni-1.0.0/productdetails.php
@@ -10,14 +10,39 @@
 <?php
 include('header.php')
     ?>
+<style>
+    .custom-button {
+        background-color: #d44620;
+        color: white;
+        border: 3px solid #d44620;
+        transition: background-color 0.3s ease;
+        border-radius: 10px;
+        /* margin-left: 60px; */
 
+    }
+
+    .custom-button:hover {
+        background-color: #a76d49;
+        /* Màu nâu */
+        color: white;
+        border: 3px solid #a76d49;
+        /* Để chữ trở nên rõ hơn trên nền nâu */
+
+    }
+
+    .custom-button img {
+        color: #983d1e;
+        width: 24px;
+        height: 24px;
+    }
+</style>
 <!-- Start Hero Section -->
 <div class="hero">
     <div class="container">
-        <div class="row justify-content-between">
-            <div class="col-lg-6">
+        <div class="row justify-content ">
+            <div class="col-lg-5">
                 <div class="intro-excerpt">
-                    <h1>Sản Phẩm</h1>
+                    <h1> Chi Tiết Sản Phẩm</h1>
                 </div>
             </div>
         </div>
@@ -27,37 +52,41 @@ include('header.php')
 
 <div class="untree_co-section">
     <div class="container">
-        <div class="row">
-            <div class="col-md-5 mb-5 mb-md-0">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-4 mb-2 mb-md-0">
                 <div class="p-2 p-lg-4 border bg-white ">
                     <div class="row">
-                        <img src="images/CayCamNgotCuaToi.png" style="width: 485px;height: 561px;"
+                        <img src="images/CayCamNgotCuaToi.png" style="width: 400px;height: 538px;"
                             class="img-fluid product-thumbnail">
                     </div>
                     <div class="row">
-                        <div class="col-md-8">
-                            <button class=" btn btn-primary mt-4 mb-4 " onclick="window.location='#'"
-                                style="border-radius: 10px;"><img src="images/cart.svg"> Thêm
+                        <div class="col-md-12 text-center">
+                            <button class=" btn custom-button mt-4    " onclick="window.location='#'"><img
+                                    src="images/cart.svg"> Thêm
                                 Vào Giỏ Hàng</button>
                         </div>
-                        <div class="col-md-4">
-                            <button class=" btn btn-primary mt-4 mb-4" onclick="window.location='#'"
-                                style="border-radius: 10px;">Quay
-                                về</button>
-                        </div>
+
 
                     </div>
                 </div>
 
             </div>
-            <div class="col-md-7">
+            <div class="col-md-8">
                 <div class="col-md-12">
                     <div class="p-2 p-lg-4 border bg-white">
                         <h2>Cây Cam Ngọt Của Tôi</h2>
-                        <p>Tác giả: <b>José Mauro de Vasconcelos</b></p>
-                        <p>Nhà xuất bản: <b>NXB Hội Nhà Văn</b></p>
-                        <h4 class="text-success">Giá thuê: <b>17.000 VNĐ</b></h4>
-                        <h4 class="text-success">Tiền cọc: <b>17.000 VNĐ</b></h4>
+                        <div class="row">
+                            <div class="col-6">
+                                <p>Tác giả: <b>José Mauro de Vasconcelos</b></p>
+                            </div>
+                            <div class="col-6">
+                                <p>Nhà xuất bản: <b>NXB Hội Nhà Văn</b></p>
+
+                            </div>
+                        </div>
+
+                        <h4 class="text-success">Giá thuê: 17.000 VND</h4>
+                        <h4 class="text-success">Tiền cọc: 187.000 VND</h4>
                         <div class="mb-3">
                             <label for="quantity" class="form-label">Số lượng:</label>
                             <input type="number" id="quantity" class="form-control" value="1" min="1"
@@ -137,7 +166,7 @@ include('header.php')
                         mươi quốc gia và chuyển thể thành phim điện ảnh. Ngoài ra, José còn rất thành công trong
                         vai
                         trò diễn viên điện ảnh và biên kịch.<br>
-                        ---------------------------------------------------------------------------------------------------------------------------------------------------------<br>
+
 
                     </p>
 
