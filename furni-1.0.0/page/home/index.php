561b0d114f9aacc0f0752ffbb4a56d728c868ba7
KingHuy <huydaden41@gmail.com>
2024-11-17 23:14:52 +0700

    Update tam thoi dua qua branch Huy3

--- furni-1.0.0/page/home/index.php
+++ furni-1.0.0/page/home/index.php
@@ -2,7 +2,7 @@
 * Bootstrap 5
 * Template Name: Furni
 * Template Author: Untree.co
-* Template URI: https://untree.co/
+* Template URI: https://untree.co
 * License: https://creativecommons.org/licenses/by/3.0/
 */ -->
 
