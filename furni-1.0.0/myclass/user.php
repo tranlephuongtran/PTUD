5d019f1a07108bdb3511876011d40f84dda55335
KingHuy <huydaden41@gmail.com>
2024-11-18 20:25:26 +0700

    [#08 Update page register, login and fix few homepage vẫn còn đưa sách trên CSDL xuống sáng mai gắng xong và xem thêm trường hợp biểu thức chính quy]

--- furni-1.0.0/myclass/user.php
+++ furni-1.0.0/myclass/user.php
@@ -1,76 +1,76 @@
 <?php
 require_once 'connect.php';
 
-class User {
+class User
+{
     private $db;
     private $conn;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->db = new Database();
         $this->conn = $this->db->conn;
     }
 
-    public function register($hoTen, $tenDangNhap, $email, $password, $confirmPassword, $sdt, $diachi, $agree) {
-        // Kiểm tra các trường bắt buộc
-        if (!$agree) {
-            echo '<script lang="javascript">
-                                alert("Bạn phải đồng ý với các chính sách và điều khoản.");
-                            </script>';
-            exit;
-        }
+    public function register($hoTen, $tenDangNhap, $email, $password, $confirmPassword, $sdt, $diachi, $agree)
+    {
+        if (!empty($hoTen) && !empty($tenDangNhap) && !empty($email) && !empty($sdt) && !empty($password) && !empty($confirmPassword)) {
 
-        if ($password !== $confirmPassword) {
-            echo '<script lang="javascript">
-                                alert("Mật khẩu và xác nhận mật khẩu không khớp.");
-                            </script>';
-            exit;
-        }
 
-        // Mã hóa mật khẩu
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-        // Kiểm tra email đã tồn tại chưa
-        $email_check_query = "SELECT * FROM taikhoan WHERE email='$email' LIMIT 1";
-        $result = $this->conn->query($email_check_query);
-        $user = $result->fetch_assoc();
-
-        if ($user) { // Nếu email đã tồn tại
-            echo '<script lang="javascript">
+            // Kiểm tra các trường bắt buộc
+            $email_check_query = "SELECT * FROM taikhoan WHERE email='$email' LIMIT 1";
+            $result = $this->conn->query($email_check_query);
+            $user = $result->fetch_assoc();
+            // Kiểm tra email đã tồn tại chưa
+            if ($user) { // Nếu email đã tồn tại
+                echo '<script lang="javascript">
                                 alert("Email đã được sử dụng. Vui lòng sử dụng email khác.");
                             </script>';
-            exit;
-        } else {
-            // Lưu thông tin vào bảng nguoidung
-            $nguoidung_query = "INSERT INTO nguoidung (ten, email, SDT, diaChi) VALUES ('$hoTen', '$email', '$sdt', '$diachi')";
-            if ($this->conn->query($nguoidung_query) === TRUE) {
-                $maNguoiDung = $this->conn->insert_id;
-                
-                // Lưu thông tin vào bảng taikhoan
-                $taikhoan_query = "INSERT INTO taikhoan (email, Password, nguoiDungID) VALUES ('$email', '$hashed_password', '$maNguoiDung')";
-                if ($this->conn->query($taikhoan_query) === TRUE) {
-                    echo '<script lang="javascript">
-                                alert("Đăng ký thành công!");
+            } elseif ($password !== $confirmPassword) {
+                echo '<script lang="javascript">
+                                alert("Mật khẩu và xác nhận mật khẩu không khớp.");
                             </script>';
-                    exit;
-                } else {
-                    // return "Lỗi: " . $taikhoan_query . "<br>" . $this->conn->error;
-                    echo '<script lang="javascript">
+            } elseif (!$agree) {
+                echo '<script lang="javascript">
+                                alert("Bạn phải đồng ý với các chính sách và điều khoản.");
+                            </script>';
+            } else {
+                // Lưu thông tin vào bảng nguoidung
+                $nguoidung_query = "INSERT INTO nguoidung (ten, email, SDT, diaChi) VALUES ('$hoTen', '$email', '$sdt', '$diachi')";
+                if ($this->conn->query($nguoidung_query) === TRUE) {
+                    $maNguoiDung = $this->conn->insert_id;
+
+                    // Lưu thông tin vào bảng taikhoan
+                    $taikhoan_query = "INSERT INTO taikhoan (email, Password, nguoiDungID) VALUES ('$email', '$password', '$maNguoiDung')";
+                    if ($this->conn->query($taikhoan_query) === TRUE) {
+                        echo '<script lang="javascript">
+                                alert("Đăng ký thành công!");
+                                window.location.href = "index.php?login";
+                            </script>';
+                    } else {
+                        // return "Lỗi: " . $taikhoan_query . "<br>" . $this->conn->error;
+                        echo '<script lang="javascript">
                                 alert("Lỗi: " . $taikhoan_query . "<br>" . $this->conn->error);
                             </script>';
-                    exit;
-                }
-            } else {
-                // return "Lỗi: " . $nguoidung_query . "<br>" . $this->conn->error;
-                echo '<script lang="javascript">
+                    }
+                } else {
+                    // return "Lỗi: " . $nguoidung_query . "<br>" . $this->conn->error;
+                    echo '<script lang="javascript">
                                 alert("Lỗi: " . $nguoidung_query . "<br>" . $this->conn->error);
                             </script>';
-                exit;
+                }
             }
+        } else {
+            echo '<script lang="javascript">
+                                alert("Vui lòng nhập đầy đủ thông tin");
+                            </script>';
         }
+        // Mã hóa mật khẩu
+        // $hashed_password = password_hash($password, PASSWORD_DEFAULT);
     }
 
-    public function __destruct() {
+    public function __destruct()
+    {
         $this->db->close();
     }
 }
-?>
